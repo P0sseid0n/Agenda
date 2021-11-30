082e36efd5e1c6c2a7d3765488542eaa7bd96e59
P0sseid0n <matheuspossidoniom@outlook.com>
2021-11-30 13:13:06 -0300

    Adicionada proteção contra csrf

--- middleware.js
+++ middleware.js
@@ -1,3 +1,9 @@
 module.exports.auth = (req, res, next) => {
-	return res.render('login_register')
+	return res.render('login_register', { csrfToken: req.csrfToken() })
+}
+
+module.exports.csurfCheck = (err, req, res, next) => {
+	if (err && 'EBADCSRFTOKEN' === err.code) return res.status(404).render('Erro', { erro: 404 })
+
+	next()
 }
--- routes.js
+++ routes.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const router = express.Router()
-const { auth } = require('./middleware')
+
+const csurf = require('csurf')
+const session = require('express-session')
+
+const { auth, csurfCheck } = require('./middleware')
+
+router.use(
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 24 * 7,
+			httpOnly: true,
+		},
+	})
+)
+
+router.use(csurf())
+
+router.post('/login', (req, res) => {})
 
 router.get('/', auth, (req, res) => {
 	res.render('index')
 })
 
 router.get('*', (req, res) => {
-	res.status(404).render('404', { erro: 404 })
+	res.status(404).render('Erro', { erro: 404 })
 })
 
 module.exports = router
